580510f6afe63502deb983256798e273f144688b
Akhil <imakhilravindran@gmail.com>
2022-04-03 22:56:41 +0530

    feat: swagger configaration added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // validation configuration
   app.useGlobalPipes(new ValidationPipe())
+
+  // swagger configuration
+  const config = new DocumentBuilder().setTitle('Smartlink API')
+    .setDescription('Smartlink API swagger documentation')
+    .setVersion('1.0.0')
+    .build()
+
+  const documentation = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/', app, documentation)
 
   await app.listen(3000);
 }
