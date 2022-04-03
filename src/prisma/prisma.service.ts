930bb2e0c254038dceb108fd9f478ca66b2252e4
Akhil <imakhilravindran@gmail.com>
2022-04-03 21:49:30 +0530

    feat: configure prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor(){
+        super({
+            datasources: {
+                db: {
+                    url: 'postgresql://postgres:password@localhost:5432/smarklink-dev?schema=public'
+                }
+            }
+        })
+    }
+
+    async onModuleInit() {
+        await this.$connect() 
+    }
+
+    async onModuleDestroy() {
+       await this.$disconnect() 
+    }
+}
